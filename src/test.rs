d6f2bb018de55c48ace4dddda2587cea891aca93
Tommy Yu <tommy.yu@auckland.ac.nz>
2022-11-11 17:54:14 +1300

    Modify test commit creation to keep building
    - Will now reuse existing Oids found in trees and their trees such that
      new commits will have a root tree that will contain _all_ previous
      blobs/trees/commits.
    - May need another enum to remove previous entries, but intended use
      cases (currently) shouldn't need that.

--- src/test.rs
+++ src/test.rs
@@ -7,6 +7,7 @@ use git2::{
     RepositoryInitOptions,
     Signature,
     Time,
+    Tree,
     TreeBuilder,
 };
 
@@ -75,42 +76,52 @@ pub fn commit(
     (commit, tree_id)
 }
 
-fn build_tree_from_objects(
+fn append_tree_from_objects(
     repo: &Repository,
-    tree: &mut TreeBuilder,
+    tree: Option<&Tree>,
+    builder: &mut TreeBuilder,
     gitobj: GitObj,
 ) {
     match gitobj {
         GitObj::Blob(name, contents) => {
             let oid = repo.blob(contents.as_bytes()).unwrap();
-            tree.insert(name, oid, 0o100644).unwrap();
+            builder.insert(name, oid, 0o100644).unwrap();
         },
         GitObj::Commit(name, contents) => {
             // TODO correct this to be an commit actually
             let oid = Oid::from_str(contents).unwrap();
-            tree.insert(name, oid, 0o160000).unwrap();
+            builder.insert(name, oid, 0o160000).unwrap();
         },
         GitObj::Tree(name, objects) => {
-            let mut subtree = repo.treebuilder(None).unwrap();
+            let subtree = &tree.map_or(None, |t| t.get_path(Path::new(&name))
+                .map_or(None, |t| t.to_object(&repo)
+                    .map_or(None, |t| t.into_tree().ok())));
+
+            let mut subbuilder = repo.treebuilder(subtree.as_ref()).unwrap();
             for object in objects {
-                build_tree_from_objects(&repo, &mut subtree, object);
+                append_tree_from_objects(
+                    &repo, subtree.as_ref(), &mut subbuilder, object);
             }
-            let oid = subtree.write().unwrap();
-            tree.insert(name, oid, 0o040000).unwrap();
+            let oid = subbuilder.write().unwrap();
+            builder.insert(name, oid, 0o040000).unwrap();
         }
     }
 }
 
-pub fn build_commit_from_objects(
+pub fn append_commit_from_objects(
     repo: &Repository,
-    objects: Vec<GitObj>,
     timestamp: Option<i64>,
+    message: Option<&str>,
+    objects: Vec<GitObj>,
 ) -> (Oid, Oid) {
-    let mut root = repo.treebuilder(None).unwrap();
+    let head_id = repo.refname_to_id("HEAD").unwrap();
+    let parent = repo.find_commit(head_id).unwrap();
+    let tree = parent.tree().unwrap();
+    let mut builder = repo.treebuilder(Some(&tree)).unwrap();
     for objects in objects {
-        build_tree_from_objects(&repo, &mut root, objects);
+        append_tree_from_objects(&repo, Some(&tree), &mut builder, objects);
     }
-    let tree_id = root.write().unwrap();
+    let tree_id = builder.write().unwrap();
     let tree = repo.find_tree(tree_id).unwrap();
     let sig = match timestamp {
         Some(ts) => Signature::new(
@@ -118,19 +129,38 @@ pub fn build_commit_from_objects(
         None => Signature::now(
             "user", "user@example.com").unwrap(),
     };
-    let head_id = repo.refname_to_id("HEAD").unwrap();
-    let parent = repo.find_commit(head_id).unwrap();
     let commit = repo.commit(
-        Some("HEAD"), &sig, &sig, "commit", &tree, &[&parent]).unwrap();
+        Some("HEAD"), &sig, &sig, message.unwrap_or("commit"),
+        &tree, &[&parent],
+    ).unwrap();
     (commit, tree_id)
 }
 
 #[test]
-fn test_build_commit_from_objects() {
+fn smoke_test_append_commit_from_objects() {
+    fn assert_blob(repo: &Repository, path: &str, answer: &str) {
+        let tree = repo.revparse_single("HEAD").unwrap()
+            .as_commit().unwrap()
+            .tree().unwrap();
+        let file = tree.get_path(Path::new(&path)).unwrap();
+        let object = file.to_object(&repo).unwrap();
+        let blob = object.as_blob().unwrap();
+        assert_eq!(std::str::from_utf8(blob.content()).unwrap(), answer);
+    }
+
+    fn assert_commit(repo: &Repository, path: &str, answer: &str) {
+        let tree = repo.revparse_single("HEAD").unwrap()
+            .as_commit().unwrap()
+            .tree().unwrap();
+        let target = tree.get_path(Path::new(&path)).unwrap();
+        assert_eq!(target.id(), Oid::from_str(answer).unwrap());
+    }
+
     let (td, repo) = crate::test::repo_init(
         None, None, true, Some(1666666666));
-    let (commit, tree_id) = crate::test::build_commit_from_objects(
-        &repo, vec![
+    let (commit, tree_id) = crate::test::append_commit_from_objects(
+        &repo, Some(1666666700), None,
+        vec![
             crate::test::GitObj::Blob("some_file", "a blob"),
             crate::test::GitObj::Tree("some_dir", vec![
                 crate::test::GitObj::Blob("file1", "file1 in some_dir"),
@@ -143,7 +173,6 @@ fn test_build_commit_from_objects() {
             crate::test::GitObj::Commit(
                 "some_gitmodule", "0123456789012345678012345678012345678901"),
         ],
-        Some(1666666700),
     );
 
     let path = &td.unwrap();
@@ -161,24 +190,36 @@ fn test_build_commit_from_objects() {
         "7e0875ba237c0897e5cda37dade7fe58fbc92447",
     );
 
-    let tree = repo_check.revparse_single("HEAD").unwrap()
-        .as_commit().unwrap()
-        .tree().unwrap();
-    let file = tree.get_path(Path::new("some_dir/nested/file_a")).unwrap();
-    let object = file.to_object(&repo_check).unwrap();
-    let blob = object.as_blob().unwrap();
-    assert_eq!(
-        std::str::from_utf8(blob.content()).unwrap(),
-        "file_a in nested",
-    );
+    assert_blob(&repo_check, "some_dir/nested/file_a", "file_a in nested");
 
     // This won't actually resolve to any valid submodule given the
     // above example construct due to the lack of `.gitmodules` file,
     // but given this is a bare repo the test is to be sure that this
     // commit object reference is injected.
-    let target = tree.get_path(Path::new("some_gitmodule")).unwrap();
-    assert_eq!(
-        format!("{}", target.id()),
-        "0123456789012345678012345678012345678901",
+    assert_commit(
+        &repo_check, "some_gitmodule",
+        "0123456789012345678012345678012345678901");
+
+    let (_, _) = crate::test::append_commit_from_objects(
+        &repo_check, Some(1666666800), None,
+        vec![
+            crate::test::GitObj::Blob("new_file", "another_file"),
+            crate::test::GitObj::Tree("some_dir", vec![
+                crate::test::GitObj::Blob("file2", "file2 modified"),
+                crate::test::GitObj::Blob("file3", "file3 is new"),
+                crate::test::GitObj::Tree("nested", vec![
+                    crate::test::GitObj::Blob("file_a", "file_a modified"),
+                    crate::test::GitObj::Blob("file_c", "file_c is new"),
+                ]),
+            ]),
+        ],
     );
+
+    assert_blob(&repo_check, "some_dir/nested/file_a", "file_a modified");
+    assert_blob(&repo_check, "some_dir/nested/file_b", "file_b in nested");
+    assert_blob(&repo_check, "some_dir/nested/file_c", "file_c is new");
+    assert_commit(
+        &repo_check, "some_gitmodule",
+        "0123456789012345678012345678012345678901");
+
 }
