cba34487c675f91f8c0eee59216909f30c93ddce
Tommy Yu <tommy.yu@auckland.ac.nz>
2023-03-30 14:39:42 +1300

    Make use of dotenv to streamline usage
    - Also provide a default .env.

--- pmrrepo/src/bin/pmrrepo.rs
+++ pmrrepo/src/bin/pmrrepo.rs
@@ -105,6 +105,7 @@ fn fetch_envvar(key: &str) -> anyhow::Result<String> {
 #[async_std::main]
 #[paw::main]
 async fn main(args: Args) -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
     stderrlog::new()
         .module(module_path!())
         .verbosity(args.verbose + 1)
