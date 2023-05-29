b5d86e95165daeb76881f74326dea9ce0e430c59
Tommy Yu <tommy.yu@auckland.ac.nz>
2023-05-29 20:35:10 +1200

    Rename build_arg

--- pmrmodel/src/model/task.rs
+++ pmrmodel/src/model/task.rs
@@ -40,7 +40,7 @@ impl Display for ArgumentError {
 // TODO handle arg.join_flag
 // TODO handle internal choices (e.g. None value override from choice)
 // TODO handle external choices (additional argument?)
-fn build_arg(
+fn value_to_taskarg(
     value: Option<&str>,
     arg: &TaskTemplateArg,
 ) -> Result<Vec<String>, ArgumentError> {
@@ -115,7 +115,7 @@ choice_fixed - if true, the provided value for task must be one of the choices
 
 
 #[test]
-fn test_build_arg_standard_no_choices() {
+fn test_value_to_taskarg_standard_no_choices() {
     let default = TaskTemplateArg { .. Default::default() };
     let none_none_default = TaskTemplateArg {
         default: Some("just a default value".into()),
@@ -164,122 +164,122 @@ fn test_build_arg_standard_no_choices() {
 
     // default
     assert_eq!(
-        build_arg(None, &default),
+        value_to_taskarg(None, &default),
         Ok([].into()),
     );
     assert_eq!(
-        &build_arg(None, &none_none_default).unwrap(),
+        &value_to_taskarg(None, &none_none_default).unwrap(),
         &["just a default value"],
     );
     assert_eq!(
-        &build_arg(None, &none_flag_none).unwrap(),
+        &value_to_taskarg(None, &none_flag_none).unwrap(),
         &["--flag"],
     );
     assert_eq!(
-        &build_arg(None, &none_flag_default).unwrap(),
+        &value_to_taskarg(None, &none_flag_default).unwrap(),
         &["--flag", "flagged default value"],
     );
 
     // unexpected values (from user input)
     assert_eq!(
-        build_arg(Some("foo"), &default),
+        value_to_taskarg(Some("foo"), &default),
         Err(ArgumentError::UnexpectedValue),
     );
     assert_eq!(
-        build_arg(Some("foo"), &none_none_default),
+        value_to_taskarg(Some("foo"), &none_none_default),
         Err(ArgumentError::UnexpectedValue),
     );
     assert_eq!(
-        build_arg(Some("foo"), &none_flag_none),
+        value_to_taskarg(Some("foo"), &none_flag_none),
         Err(ArgumentError::UnexpectedValue),
     );
     assert_eq!(
-        build_arg(Some("foo"), &none_flag_default),
+        value_to_taskarg(Some("foo"), &none_flag_default),
         Err(ArgumentError::UnexpectedValue),
     );
 
     // prompted, no response
     assert_eq!(
-        build_arg(None, &prompt_none_none),
+        value_to_taskarg(None, &prompt_none_none),
         Err(ArgumentError::ValueExpected),
     );
     assert_eq!(
-        &build_arg(None, &prompt_none_default).unwrap(),
+        &value_to_taskarg(None, &prompt_none_default).unwrap(),
         &["prompted but have default value"],
     );
     assert_eq!(
-        &build_arg(None, &prompt_none_dempty).unwrap(),
+        &value_to_taskarg(None, &prompt_none_dempty).unwrap(),
         &[""],
     );
     assert_eq!(
-        build_arg(None, &prompt_flag_none),
+        value_to_taskarg(None, &prompt_flag_none),
         Err(ArgumentError::ValueExpected),
     );
     assert_eq!(
-        &build_arg(None, &prompt_flag_default).unwrap(),
+        &value_to_taskarg(None, &prompt_flag_default).unwrap(),
         &["-P", "prompted and flagged default value"],
     );
     assert_eq!(
-        &build_arg(None, &prompt_flag_dempty).unwrap(),
+        &value_to_taskarg(None, &prompt_flag_dempty).unwrap(),
         &["-P", ""],
     );
 
     // prompted with non-empty string response
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_none_none).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_none_none).unwrap(),
         &["user value"],
     );
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_none_default).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_none_default).unwrap(),
         &["user value"],
     );
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_none_dempty).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_none_dempty).unwrap(),
         &["user value"],
     );
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_flag_none).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_flag_none).unwrap(),
         &["-P", "user value"],
     );
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_flag_default).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_flag_default).unwrap(),
         &["-P", "user value"],
     );
     assert_eq!(
-        &build_arg(Some("user value"), &prompt_flag_dempty).unwrap(),
+        &value_to_taskarg(Some("user value"), &prompt_flag_dempty).unwrap(),
         &["-P", "user value"],
     );
 
     // prompted with non-empty string response
     assert_eq!(
-        build_arg(Some(""), &prompt_none_none),
+        value_to_taskarg(Some(""), &prompt_none_none),
         Err(ArgumentError::ValueExpected),
     );
     assert_eq!(
-        &build_arg(Some(""), &prompt_none_default).unwrap(),
+        &value_to_taskarg(Some(""), &prompt_none_default).unwrap(),
         &["prompted but have default value"],
     );
     assert_eq!(
-        &build_arg(Some(""), &prompt_none_dempty).unwrap(),
+        &value_to_taskarg(Some(""), &prompt_none_dempty).unwrap(),
         &[""],
     );
     assert_eq!(
-        build_arg(Some(""), &prompt_flag_none),
+        value_to_taskarg(Some(""), &prompt_flag_none),
         Err(ArgumentError::ValueExpected),
     );
     assert_eq!(
-        &build_arg(Some(""), &prompt_flag_default).unwrap(),
+        &value_to_taskarg(Some(""), &prompt_flag_default).unwrap(),
         &["-P", "prompted and flagged default value"],
     );
     assert_eq!(
-        &build_arg(Some(""), &prompt_flag_dempty).unwrap(),
+        &value_to_taskarg(Some(""), &prompt_flag_dempty).unwrap(),
         &["-P", ""],
     );
 
 }
 
 #[test]
-fn test_build_arg_standard_choices() {
+fn test_value_to_taskarg_standard_choices() {
     let none_none = TaskTemplateArg {
         choice_source: Some("".into()),
         .. Default::default()
@@ -302,19 +302,19 @@ fn test_build_arg_standard_choices() {
     };
 
     assert_eq!(
-        build_arg(None, &none_none),
+        value_to_taskarg(None, &none_none),
         Ok([].into()),
     );
     assert_eq!(
-        build_arg(None, &none_flag).unwrap(),
+        value_to_taskarg(None, &none_flag).unwrap(),
         &["--flag"],
     );
     assert_eq!(
-        build_arg(None, &prompt_none),
+        value_to_taskarg(None, &prompt_none),
         Ok([].into()),
     );
     assert_eq!(
-        build_arg(None, &prompt_flag).unwrap(),
+        value_to_taskarg(None, &prompt_flag).unwrap(),
         &["-P"],
     );
 
