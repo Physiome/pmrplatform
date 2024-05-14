8ed54784d9d37cdbf76de9821a9ced850a19da83
Tommy Yu <tommy.yu@auckland.ac.nz>
2024-05-14 17:31:39 +1200

    Stale task tested.

--- pmrctrl/tests/pmrctrl_test.rs
+++ pmrctrl/tests/pmrctrl_test.rs
@@ -1045,9 +1045,8 @@ async fn test_exposure_file_view_task_run_view_key_success() -> anyhow::Result<(
         .await?
         .expect("task was queued");
     assert_eq!(task.id(), task_id);
-    // pretend we ran it and complete it
     task.run(12345).await?;
-    // TODO test for other status codes
+
     let result = platform.complete_task(task, 0).await?;
     assert!(result);
 
@@ -1060,7 +1059,7 @@ async fn test_exposure_file_view_task_run_view_key_success() -> anyhow::Result<(
 }
 
 #[async_std::test]
-async fn test_exposure_file_view_task_run_view_key_task_fail() -> anyhow::Result<()> {
+async fn test_exposure_file_view_task_run_task_fail() -> anyhow::Result<()> {
     let (_reporoot, platform) = create_sqlite_platform().await?;
     let vtts = make_example_view_task_templates(&platform).await?;
     let exposure = platform.create_exposure(
@@ -1105,7 +1104,71 @@ async fn test_exposure_file_view_task_run_view_key_task_fail() -> anyhow::Result
         .await?;
     assert_eq!(efv.view_key(), None);
 
-    // TODO test for task queue after this started.
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_exposure_file_view_task_run_task_stale() -> anyhow::Result<()> {
+    let (_reporoot, platform) = create_sqlite_platform().await?;
+    let vtts = make_example_view_task_templates(&platform).await?;
+    let exposure = platform.create_exposure(
+        1,
+        "083b775d81ec9b66796edbbdce4d714bb2ddc355",
+    ).await?;
+    let efc = exposure.create_file("if1").await?;
+
+    ExposureTaskTemplateBackend::set_file_templates(
+        platform.mc_platform.as_ref(),
+        efc.exposure_file().id(),
+        [vtts[0]].into_iter(),
+    ).await?;
+    let efvttsc = efc.build_vttc().await?;
+    let user_input = UserInputMap::from([
+        (1, "Example answer".to_string()),
+    ]);
+
+    let tasks = efvttsc.create_tasks_from_input(&user_input)?;
+    let result = efc.process_vttc_tasks(tasks).await?;
+    let (exposure_file_view_id, task_id) = result[0];
+
+    let efv = platform.mc_platform.as_ref()
+        .get_exposure_file_view(exposure_file_view_id)
+        .await?;
+    assert_eq!(efv.view_key(), None);
+
+    // spawn a task
+    let mut task = platform.tm_platform.as_ref()
+        .start_task()
+        .await?
+        .expect("task was queued");
+    assert_eq!(task.id(), task_id);
+
+    // record another task being queued.
+    efc.process_vttc_tasks(
+        efvttsc.create_tasks_from_input(&user_input)?,
+    ).await?;
+
+    // now run the task, that is now stale...
+    task.run(12345).await?;
+    // ... even if it was started later.
+    let later_task = platform.tm_platform.as_ref()
+        .start_task()
+        .await?
+        .expect("task was queued");
+
+    let result = platform.complete_task(task, 0).await?;
+    assert!(!result);
+    // That shouldn't set the view just yet.
+    let efv = platform.mc_platform.as_ref()
+        .get_exposure_file_view(exposure_file_view_id)
+        .await?;
+    assert_eq!(efv.view_key(), None);
+
+    assert!(platform.complete_task(later_task, 0).await?);
+    let efv = platform.mc_platform.as_ref()
+        .get_exposure_file_view(exposure_file_view_id)
+        .await?;
+    assert_eq!(efv.view_key(), Some("example_view1"));
 
     Ok(())
 }
