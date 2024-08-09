d216356fdce5f23b1a927232ed09e291f9e11eb4
Tommy Yu <tommy.yu@auckland.ac.nz>
2024-08-09 14:27:09 +1200

    Removing `pmrapp_client` and `pmrapp_server`
    - Thanks `sauron`, and long live `leptos`.

--- pmrapp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-cd "$(dirname "$0")"
-
-# This script will build and run the pmrapp_server.
-
-wasm-pack build pmrapp_client --release --target web
-cargo run --release --bin pmrapp_server --package pmrapp_server
