0fd656b12e7028373e04ef2187ef6986bd0776af
Tommy Yu <tommy.yu@auckland.ac.nz>
2024-10-10 01:33:14 +1300

    Allow a lengthier period be produced.
    - This is done by accepting two arguments to produce two cycles which
      will be xor'd together.
    - The default implementation will produce pseudorandom looking outputs
      with a period of 301 (product of the lengths of the inputs, which are
      43 and 7).

--- testing/src/rand.rs
+++ testing/src/rand.rs
@@ -4,20 +4,39 @@ use std::{
     vec::IntoIter,
 };
 
+/// A crappy Rng that xors two different cycles of `u8`s together.  If
+/// they are of different prime number lengths they may have a slightly
+/// longer cycle than just working with one cycle.  Only suitable for
+/// generating test values, hilariously unsuitable for anything that
+/// requires any notion of security.
 pub struct MockRng {
-    cycle: Cycle<IntoIter<u8>>,
+    cycle1: Cycle<IntoIter<u8>>,
+    cycle2: Cycle<IntoIter<u8>>,
 }
 
 impl MockRng {
-    pub fn new(data: impl Into<Vec<u8>>) -> Self {
-        let cycle = data.into()
+    pub fn default() -> Self {
+        Self::new(
+            "the quick brown fox jumps over the lazy fox",
+            [204, 240, 170, 85, 15, 51, 219],
+        )
+    }
+
+    pub fn new(data1: impl Into<Vec<u8>>, data2: impl Into<Vec<u8>>) -> Self {
+        let cycle1 = data1.into()
             .into_iter()
             .cycle();
-        Self { cycle }
+        let cycle2 = data2.into()
+            .into_iter()
+            .cycle();
+        Self { cycle1, cycle2 }
     }
 
     pub fn next(&mut self) -> u8 {
-        self.cycle
+        self.cycle1
+            .next()
+            .unwrap_or(0) ^
+        self.cycle2
             .next()
             .unwrap_or(0)
     }
@@ -54,8 +73,9 @@ mod tests {
 
     #[test]
     fn smoke() {
-        let mut rng = MockRng::new("0");
+        let mut rng = MockRng::new("0", "");
         assert_eq!(rng.gen::<i8>(), 48);
+        assert_eq!(rng.gen::<i32>(), 808464432);
         assert_eq!(rng.gen::<i64>(), 3472328296227680304);
         assert_eq!(rng.gen::<i128>(), 64053151420411946063694043751862251568);
 
@@ -64,7 +84,19 @@ mod tests {
             .expect("infallable");
         assert_eq!(slice, [48, 48, 48]);
 
-        let mut zero = MockRng::new("");
+        let mut zero = MockRng::new("", "");
         assert_eq!(zero.gen::<i64>(), 0);
+        assert_eq!(zero.gen::<i64>(), 0);
+
+        let mut one = MockRng::new("0", "1");
+        assert_eq!(one.gen::<i8>(), 1);
+        assert_eq!(one.gen::<i8>(), 1);
+        assert_eq!(one.gen::<i32>(), 16843009);
+
+        // sequence repeats in 15 (product of lengths of the two inputs).
+        let mut xor = MockRng::new([45, 76, 182], [251, 189, 49, 128, 7]);
+        let mut repeats = [0u8; 30];
+        xor.fill_bytes(repeats.as_mut_slice());
+        assert_eq!(repeats[..15], repeats[15..])
     }
 }
