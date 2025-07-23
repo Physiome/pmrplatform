4a28590217c1d6d580a6f7b3444b5b8e76fbd86a
Tommy Yu <tommy.yu@auckland.ac.nz>
2025-07-24 05:50:37 +1200

    Correct test when run without session feature

--- pmrcore/tests/test_types.rs
+++ pmrcore/tests/test_types.rs
@@ -1,11 +1,14 @@
-use pmrcore::ac::session::{
-    SessionFactory,
-    SessionTokenFactory,
-};
-use test_pmr::is_send_sync;
+#[cfg(feature = "server")]
+mod session {
+    use pmrcore::ac::session::{
+        SessionFactory,
+        SessionTokenFactory,
+    };
+    use test_pmr::is_send_sync;
 
-#[test]
-fn check_send_sync() {
-    is_send_sync::<SessionFactory>();
-    is_send_sync::<SessionTokenFactory>();
+    #[test]
+    fn check_send_sync() {
+        is_send_sync::<SessionFactory>();
+        is_send_sync::<SessionTokenFactory>();
+    }
 }
