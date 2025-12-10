82e87c6ec71a95ad52566835110cee0169eae63c
Tommy Yu <tommy.yu@auckland.ac.nz>
2025-12-11 02:09:23 +1300

    Provide dedicated binary to index CellML files
    - This one reuses the same store to query for the pubmed id along with
      keywords.

---
+++ pmrmeta/src/bin/pmr-cellml.rs
@@ -0,0 +1,92 @@
+use clap::{Parser, Subcommand};
+use pmrmeta::{
+    cellml::meta::{
+        query_keywords,
+        query_pubmed_id,
+    },
+    read::xml_to_store,
+};
+use pmrctrl::platform::Builder as PlatformBuilder;
+use std::{
+    fs,
+    io::BufReader,
+    sync::OnceLock,
+};
+
+#[derive(Debug, Parser)]
+struct Config {
+    #[clap(short = 'v', long = "verbose", action = clap::ArgAction::Count)]
+    verbose: u8,
+}
+
+static CONF: OnceLock<Config> = OnceLock::new();
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    #[clap(flatten)]
+    platform_builder: PlatformBuilder,
+    #[clap(flatten)]
+    config: Config,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(arg_required_else_help = true)]
+    Index {
+        #[clap(long)]
+        input_path: String,
+        #[clap(long)]
+        exposure_id: i64,
+        #[clap(long)]
+        exposure_path: String,
+    },
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().ok();
+    let args = Cli::parse();
+    stderrlog::new()
+        .module(module_path!())
+        .module("pmrdb")
+        .verbosity((args.config.verbose as usize) + 1)
+        .timestamp(stderrlog::Timestamp::Second)
+        .init()
+        .unwrap();
+
+    let platform = args.platform_builder
+        .build()
+        .await
+        .map_err(anyhow::Error::from_boxed)?;
+
+    let _ = CONF.set(args.config);
+
+    match args.command {
+        Commands::Index { input_path, exposure_id, exposure_path } => {
+            let reader = BufReader::new(fs::File::open(input_path)?);
+            let resource_path = format!("/exposure/{exposure_id}/{exposure_path}");
+            let store = xml_to_store(reader)?;
+
+            // Store the citation for the incoming file
+            let citations = query_pubmed_id(&store)?;
+            for citation in citations.iter() {
+                platform.pc_platform.add_citation(&citation).await.ok();
+                platform.pc_platform.link_citation(&citation, &resource_path).await.ok();
+            }
+
+            // Add the various information acquired from the metadata into the index
+            let keywords = query_keywords(&store)?;
+            platform.pc_platform.index_resource(
+                "cellml_keyword",
+                &resource_path,
+                &mut keywords.iter()
+                    .map(String::as_ref),
+            ).await?;
+
+        }
+    }
+
+    Ok(())
+}
