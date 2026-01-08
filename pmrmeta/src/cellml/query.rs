c31b1508d08f7ff84e475d9ec5d1b175d85df1e4
Tommy Yu <tommy.yu@auckland.ac.nz>
2026-01-09 00:03:38 +1300

    Common query_item function.

--- pmrmeta/src/cellml/query.rs
+++ pmrmeta/src/cellml/query.rs
@@ -10,10 +10,43 @@ use crate::{
     read::BASE_IRI,
 };
 
-pub fn keywords(store: &Store) -> Result<Vec<String>, RdfIndexerError> {
+fn query_items<F>(
+    store: &Store,
+    query: &'static str,
+    var: &'static str,
+    value: Option<&str>,
+    formatter: F,
+) -> Result<Vec<String>, RdfIndexerError>
+where
+    F: Fn(&str) -> String,
+{
     let mut result = Vec::new();
-    if let QueryResults::Solutions(solutions) = SparqlEvaluator::new()
-        .parse_query(r#"
+    let mut query = SparqlEvaluator::new().parse_query(query)?;
+
+    if let Some(value) = value {
+        let iri = Iri::parse(BASE_IRI)?.resolve(value)?;
+        query = query.substitute_variable(
+            Variable::new("node").expect("specified static value must parse correctly"),
+            NamedNodeRef::new_unchecked(&iri),
+        );
+    }
+
+    if let QueryResults::Solutions(solutions) = query.on_store(&store).execute()? {
+        for solution in solutions {
+            if let Ok(solution) = solution {
+                if let Some(Term::Literal(literal)) = solution.get(var) {
+                    result.push(formatter(literal.value()));
+                }
+            }
+        }
+    }
+    Ok(result)
+}
+
+pub fn keywords(store: &Store) -> Result<Vec<String>, RdfIndexerError> {
+    query_items(
+        store,
+        r#"
             PREFIX bqs: <http://www.cellml.org/bqs/1.0#>
             PREFIX dc: <http://purl.org/dc/elements/1.1/>
             PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -24,25 +57,17 @@ pub fn keywords(store: &Store) -> Result<Vec<String>, RdfIndexerError> {
                 ?bqs dc:subject [ rdf:value ?container ] .
                 ?container ?li ?value .
             }
-        "#)?
-        .on_store(&store)
-        .execute()?
-    {
-        for solution in solutions {
-            if let Ok(solution) = solution {
-                if let Some(Term::Literal(literal)) = solution.get("value") {
-                    result.push(literal.value().to_string());
-                }
-            }
-        }
-    }
-    Ok(result)
+        "#,
+        "value",
+        None,
+        str::to_string,
+    )
 }
 
 pub fn pubmed_id(store: &Store) -> Result<Vec<String>, RdfIndexerError> {
-    let mut result = Vec::new();
-    if let QueryResults::Solutions(solutions) = SparqlEvaluator::new()
-        .parse_query(r#"
+    query_items(
+        store,
+        r#"
             PREFIX bqs: <http://www.cellml.org/bqs/1.0#>
 
             SELECT ?ref ?pmid
@@ -51,50 +76,27 @@ pub fn pubmed_id(store: &Store) -> Result<Vec<String>, RdfIndexerError> {
                 ?ref bqs:JournalArticle ?article .
                 OPTIONAL { ?ref bqs:Pubmed_id ?pmid } .
             }
-        "#)?
-        .on_store(&store)
-        .execute()?
-    {
-        for solution in solutions {
-            if let Ok(solution) = solution {
-                if let Some(Term::Literal(literal)) = solution.get("pmid") {
-                    result.push(format!("pmid:{}", literal.value()));
-                }
-            }
-        }
-    }
-    Ok(result)
+        "#,
+        "pmid",
+        None,
+        |pmid| format!("pmid:{pmid}"),
+    )
 }
 
 /// Return the dc:title, optionally constrained the results from the specified node.
 pub fn dc_title(store: &Store, node: Option<&str>) -> Result<Vec<String>, RdfIndexerError> {
-    let mut result = Vec::new();
-    let mut query = SparqlEvaluator::new()
-        .parse_query(r#"
+    query_items(
+        store,
+        r#"
             PREFIX dc: <http://purl.org/dc/elements/1.1/>
 
             SELECT ?node ?title
             WHERE {
                 ?node dc:title ?title .
             }
-        "#)?;
-
-    if let Some(node) = node {
-        let iri = Iri::parse(BASE_IRI)?.resolve(node)?;
-        query = query.substitute_variable(
-            Variable::new("node").expect("static correct value shouldn't fail"),
-            NamedNodeRef::new_unchecked(&iri),
-        );
-    }
-
-    if let QueryResults::Solutions(solutions) = query.on_store(&store).execute()? {
-        for solution in solutions {
-            if let Ok(solution) = solution {
-                if let Some(Term::Literal(literal)) = solution.get("title") {
-                    result.push(literal.value().to_string());
-                }
-            }
-        }
-    }
-    Ok(result)
+        "#,
+        "title",
+        node,
+        str::to_string,
+    )
 }
